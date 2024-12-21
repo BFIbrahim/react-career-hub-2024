cc4e283b45942a023f31c7136d1e02a6e1404359
Md Ibrahim <mdibrahim36194@gmail.com>
2024-12-21 14:20:19 +0600

    Featured jobs section added

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,18 +5,29 @@ const FeaturedJobs = () => {
 
     const [jobs, setJobs] = useState([]);
 
+    const [dataLength, setDataLength] = useState(4)
+
     useEffect(() => {
         fetch('jobs.json')
-        .then(res => res.json())
-        .then(jobs => setJobs(jobs))
+            .then(res => res.json())
+            .then(jobs => setJobs(jobs))
     })
 
     return (
         <div>
-            <div>
+            <div className='text-center mt-8'>
+                <h1 className='text-3xl font-sans font-bold mb-2'>Featured Jobs</h1>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div className='md:grid grid-cols-2 gap-5'>
                 {
-                    jobs.map(job => <SingleJob key={job.id} job={job}></SingleJob>)
+                    jobs.slice(0, dataLength).map(job => <SingleJob key={job.id} job={job}></SingleJob>)
                 }
+            </div>
+            <div className='text-center'>
+                <div className={dataLength === jobs.length && 'hidden'}>
+                    <button onClick={() => setDataLength(jobs.length)} className='btn mb-5 bg-sky-500 text-white hover:text-black'>Show All Jobs</button>
+                </div>
             </div>
         </div>
     );
--- src/Components/SingleJob/SingleJob.jsx
+++ src/Components/SingleJob/SingleJob.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { IoLocationOutline } from "react-icons/io5";
+import { BsCoin } from "react-icons/bs";
 
-const SingleJob = ({job}) => {
-    
+
+
+const SingleJob = ({ job }) => {
+
     return (
-        <div>
-            <p>{job.job_title}</p>
+        <div className=''>
+            <div className='mt-8 mb-8'>
+                <img src={job.logo} alt="" />
+                <h2 className='font-bold mt-5 text-2xl'>{job.job_title}</h2>
+                <h4 className='font-semibold text-gray-500 mb-4'>{job.company_name}</h4>
+                <div className='flex mb-4'>
+                    <h4 className='p-2 border-2 rounded border-sky-500 bg-transparent me-4'>{job.remote_or_onsite}</h4>
+                    <h4  className='p-2 border-2 rounded border-sky-500 bg-transparent'>{job.job_type}</h4>
+                </div>
+                <p className='flex gap-3'>
+                    <p className='flex gap-2 items-center'><IoLocationOutline className='text-sky-500'/> {job.location}</p>
+                    <p className='flex gap-2 items-center'><BsCoin className='text-sky-500'/> {job.salary}</p>
+                </p>
+
+                <button className='btn mt-3 bg-sky-500 text-white hover:text-black'>View Details</button>
+            </div>
         </div>
     );
 };
